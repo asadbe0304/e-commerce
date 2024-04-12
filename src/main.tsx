ba4fcc92e72e6939c018b9c956a1a0871efcc59b
asadbe0304 <asadbekaxmadkulov@gmail.com>
2024-04-12 21:35:11 +0500

    Settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import { Provider } from 'react-redux'
 import store from './store/store'
-
+import Route from "./Routes/index"
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import './index.css'
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <Route />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
